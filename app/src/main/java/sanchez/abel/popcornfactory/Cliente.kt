5fa56026585255f2a5b43ae00c4d1b83da6f41ee
abel2606 <abel.sanchez.guerrero@gmail.com>
2025-02-18 12:59:32 -0700

    Avance 2

---
+++ app/src/main/java/sanchez/abel/popcornfactory/Cliente.kt
@@ -0,0 +1,8 @@
+package sanchez.abel.popcornfactory
+
+data class Cliente(
+    var nombre: String,
+    var tipoPago:Int,
+    var asiento: Int) {
+
+}
--- app/src/main/java/sanchez/abel/popcornfactory/DetallePelicula.kt
+++ app/src/main/java/sanchez/abel/popcornfactory/DetallePelicula.kt
@@ -1,6 +1,8 @@
 package sanchez.abel.popcornfactory
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -23,12 +25,33 @@ class DetallePelicula : AppCompatActivity() {
         val iv_pelicula_imagen: ImageView = findViewById(R.id.iv_pelicula_imagen)
         val tv_nombre_pelicula: TextView = findViewById(R.id.tv_nombre_pelicula)
         val tv_pelicula_desc: TextView = findViewById(R.id.tv_pelicula_desc)
+        val seatsLeft:TextView = findViewById(R.id.seatsLeft)
+        val buyTickets: Button = findViewById(R.id.buyTickets)
+        var ns = 0
+        var id = -1
+        var title = "";
 
 
         if(bundle != null){
+            ns = bundle.getInt("numberSeats")
+            title = bundle.getString("titulo")!!
             iv_pelicula_imagen.setImageResource(bundle.getInt("header"))
             tv_nombre_pelicula.setText(bundle.getString("titulo"))
             tv_pelicula_desc.setText(bundle.getString("sinopsis"))
+            seatsLeft.setText("$ns seats available")
+            id = bundle.getInt("pos")
+        }
+
+        if(ns==0){
+            buyTickets.isEnabled = false
+        }
+        else{
+            buyTickets.setOnClickListener {
+                val intent: Intent = Intent(this, SeatsSelection::class.java)
+                intent.putExtra("id", id)
+                intent.putExtra("name", title)
+
+            }
         }
     }
 }
